aec7ec8e462bafb42052b594f24d2ae395a4f24e
madih <mahmoudmadih150@gamil.com>
2026-06-06 16:05:32 +0300

    even and odd task

---
+++ bin/even_odd.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main (){
+  print("Even or Odd checker");
+  print ("Enter a number:");
+ int number= int.parse(stdin.readLineSync()!);
+ if (number % 2 == 0){
+   print("Even");
+ } else {
+   print("Odd");
+ }
+
+
+}
